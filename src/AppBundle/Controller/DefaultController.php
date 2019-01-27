24d5a1ccf486f88dd3df09c60716706d417d4f36
Arturo Barba <arturo.barba@iesdonana.org>
2019-01-27 10:19:57 +0100

    Pruebas

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,59 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/list/{page}", name="list")
+     */
+    public function listAction(Request $request, $page)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => 'list',
+        ]);
+    }
+
+    /**
+     * @Route("/category/{page}", name="homepage")
+     */
+    public function categoryAction(Request $request, $page)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => 'category',
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="post_view")
+     */
+    public function postViewAction(Request $request, $slug)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => 'list',
+        ]);
+    }
+
+    /**
+     * @Route("/post/new", name="post_new")
+     */
+    public function postNewAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => 'postN',
+        ]);
+    }
+
+    /**
+     * @Route("/post/edir/{slug}", name="post_edit")
+     */
+    public function postEditAction(Request $request, $slug)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => 'list',
+        ]);
+    }
 }
